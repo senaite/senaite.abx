a47cb1c924b6f103679109c78e7d218351f995de
Jordi Puiggené <jp@naralabs.com>
2023-04-20 10:25:14 +0200

    Remove IAntibioticBehavior and use IAntibioticSchema instead

--- src/senaite/abx/upgrade/v01_02_000.py
+++ src/senaite/abx/upgrade/v01_02_000.py
@@ -18,10 +18,13 @@
 # Copyright 2020-2023 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-from senaite.core.upgrade import upgradestep
-from senaite.core.upgrade.utils import UpgradeUtils
+from bika.lims import api
+from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2Base
 from senaite.abx import logger
 from senaite.abx import PRODUCT_NAME
+from senaite.abx.content.antibiotic import Antibiotic
+from senaite.core.upgrade import upgradestep
+from senaite.core.upgrade.utils import UpgradeUtils
 
 version = "1200"
 profile = "profile-{0}:default".format(PRODUCT_NAME)
@@ -46,3 +49,39 @@ def upgrade(tool):
 
     logger.info("{0} upgraded to version {1}".format(PRODUCT_NAME, version))
     return True
+
+
+def remove_antibiotic_behavior(tool):
+    """Removes IAntibioticBehavior and uses IAntibioticSchema instead
+    """
+    logger.info("Remove IAntibioticBehavior behavior ...")
+    pt = api.get_tool("portal_types")
+    fti = pt.get("Antibiotic")
+
+    # set the new schema
+    schema = "senaite.abx.content.antibiotic.IAntibioticSchema"
+    fti.schema = schema
+
+    # remove behaviors
+    behaviors = fti.behaviors
+    to_remove = [
+        "plone.app.dexterity.behaviors.metadata.IBasic",
+        "senaite.abx.behaviors.antibiotic.IAntibioticBehavior",
+    ]
+    behaviors = filter(lambda b: b not in to_remove, behaviors)
+
+    # Re-assign behaviors
+    fti.behaviors = tuple(behaviors)
+
+    # Antibiotic content type is now folderish
+    setup = api.get_setup()
+    antibiotics = setup.antibiotics
+    for antibiotic in antibiotics.objectValues():
+        id = antibiotic.getId()
+        antibiotics._delOb(id)
+        antibiotic.__class__ = Antibiotic
+        antibiotics._setOb(id, antibiotic)
+        BTreeFolder2Base._initBTrees(antibiotics[id])
+        antibiotics[id].reindexObject()
+
+    logger.info("Remove IAntibioticBehavior behavior [DONE]")
